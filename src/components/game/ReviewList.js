e93abd9457778819de2d15e782e0dcb11cc4c787
Jaclyn French <jaclynmariefrench@gmail.com>
2021-08-12 09:29:08 -0500

    finished ch10

--- src/components/game/ReviewList.js
+++ src/components/game/ReviewList.js
@@ -4,30 +4,23 @@ import { GameContext } from "./GameProvider";
 import { ReviewContext } from "./ReviewProvider";
 
 export const ReviewList = (props) => {
-  const { reviews, getReviews, getReviewByGameId } = useContext(ReviewContext);
+  const { reviews, getReviewByGameId } = useContext(ReviewContext);
   
-  const [review, setReview] = useState()
-
-  useEffect(() => {
-    getReviews();
-  }, []);
 
   const {game_id} = useParams()
 
     useEffect(() => {
-      getReviewByGameId(parseInt(game_id)).then((r)=>{
-          setReview(r)
-        })
+      getReviewByGameId(parseInt(game_id))
     }, [])
 
   return (
     <>
       <article className="reviews">
-        {review.map((r) => {
+        {reviews?.map((r) => {
         return (
             <section key={`review--${r.id}`} className="review">
                 <div className="review__name">
-                    <h3>{r.review}</h3> by {r.player.bio} {r.game.title}
+                    <h3>{r.review}</h3> by {r.player.bio}
                 </div>
             </section>
     );
